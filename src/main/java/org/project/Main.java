e91ab1804c600766f9944ddf3081f3f49eb2bc78
Konstantin <k.bobkov1408@gmail.com>
2023-08-22 15:40:02 +0300

    Create Record class

--- src/main/java/org/project/Main.java
+++ src/main/java/org/project/Main.java
@@ -1,9 +1,5 @@
-package org.example;
+package org.project;
 
-/**
- * Hello world!
- *
- */
 public class Main
 {
     public static void main( String[] args )
---
+++ src/main/java/org/project/Record.java
@@ -0,0 +1,28 @@
+package org.project;
+
+import java.util.UUID;
+
+public class Record {
+    private final String name;
+    private String summary;
+    private final UUID id;
+
+
+    public Record(String name, String summary) {
+        this.name = name;
+        this.summary = summary;
+        id = UUID.randomUUID();
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+}
