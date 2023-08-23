38a2363ab8fffd325d23955e30aa6a420974c561
Konstantin <k.bobkov1408@gmail.com>
2023-08-23 19:46:39 +0300

    Add logger for notepad

--- src/main/java/org/project/Notepad.java
+++ src/main/java/org/project/Notepad.java
@@ -1,37 +1,46 @@
 package org.project;
 
+import org.apache.log4j.Logger;
+
 import java.util.ArrayList;
 import java.util.UUID;
 
 public class Notepad {
     private final ArrayList<Record> allRecords = new ArrayList<>();
+    private final Logger log = Logger.getLogger(Notepad.class);
 
 
     public Record addRecord(String name, String summary) {
         Record record = new Record(name, summary);
         allRecords.add(record);
+        log.info("Record "+record.getName()+" added to notepad");
         return record;
     }
 
     public ArrayList<Record> getAllRecords() {
+        log.info("All records have been received");
         return allRecords;
     }
 
     public void deleteRecordById(UUID id) {
+        log.info("Record has been deleted");
         allRecords.remove(getRecordById(id));
     }
 
     public Record getRecordById(UUID id) {
         for (Record record : allRecords) {
             if (record.getId() == id) {
+                log.info("Record was found by id");
                 return record;
             }
         }
+        log.error("Record was NOT found by id");
         return new Record("Empty", "");
     }
 
     public void deleteAllRecords() {
         allRecords.clear();
+        log.info("All records have been deleted");
     }
 
     public boolean idIsExist(UUID id) {
@@ -43,6 +52,7 @@ public class Notepad {
     }
 
     public Record getRecordByIndex(int index) {
+        log.info("Record was found by index");
         return allRecords.get(index);
     }
 
@@ -53,5 +63,6 @@ public class Notepad {
     public void editRecord(UUID id, String summary) {
         Record result = getRecordById(id);
         result.setSummary(summary);
+        log.info("Record changed successfully");
     }
 }
